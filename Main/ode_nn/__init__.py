5063d5ac84dd864a9177f72fa103d1744a9ea049
Ray <32885937+Rui1521@users.noreply.github.com>
2020-08-27 10:27:34 -0400

    Update __init__.py

--- Main/ode_nn/__init__.py
+++ Main/ode_nn/__init__.py
@@ -1,5 +1,6 @@
 #from .neural_odes import *
 from .DNN import *
 from .train import *
-from .graph_models import *
+from .Graph import *
+from .AutoODE import *
 from .torchdiffeq import odeint, odeint_adjoint
